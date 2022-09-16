3ad7b2092363d4826eeaa9a711bc4f0fc96c91d7
adelinazmarandoiu <adelinazmarandoiu@totalsoft.ro>
2022-09-16 17:26:19 +0300

    traducere

--- src/constants/menuConfig.js
+++ src/constants/menuConfig.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Dashboard } from '@material-ui/icons'
 import {People} from '@material-ui/icons'
+import {PersonAdd} from '@material-ui/icons'
 
 const menuItems = [{ icon: <Dashboard />, text: 'NavBar.Dashboard', path: '/dashboard', name: 'Dashboard' },
-{  icon: <People />, text: 'Angajati', path: '/angajati', name: 'Angajati' },{  text: 'Adauga_Angajat', path: '/adauga_angajat', name: 'Adauga_Angajat' }]
+{  icon: <People />, text: 'Angajati', path: '/angajati', name: 'Angajati' },{icon: <PersonAdd />,  text: 'NavBar.Adauga_Angajat', path: '/adauga_angajat', name: 'Adauga_Angajat' }]
 
 export default menuItems
+
