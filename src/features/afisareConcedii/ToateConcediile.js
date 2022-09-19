15900759095673458c3c89c451f3e0d6a9678e38
elena.craciun <elena.craciun@TS1542>
2022-09-19 13:31:30 +0300

    ds

--- src/features/afisareConcedii/ToateConcediile.js
+++ src/features/afisareConcedii/ToateConcediile.js
@@ -1,45 +1,6 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import TabelConcediu from './TabelConcediu';
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.success.dark,
-    color: theme.palette.common.white,
-    fontSize: 16,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
-
-// function createData(
-//   tipConcediu: string,
-//   dataInceput: string,
-//   dataSfarsit: string,
-//   inlocuitor: string,
-//   motiv: string,
-//   angajat: string,
-//   stareConcediu: string
-// ) {
-//   return { tipConcediu, dataInceput, dataSfarsit, inlocuitor, motiv, angajat, stareConcediu };
-// }
 
 const rows = [{
   "name":'Concediu medical',
@@ -69,35 +30,8 @@ const rows = [{
 
 export default function ToateConcediile() {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow >
-            <StyledTableCell align="center" style={{fontWeight: 'bold'}}>Tipul Concediului</StyledTableCell>
-            <StyledTableCell align="center" style={{fontWeight: 'bold'}}>Data de inceput</StyledTableCell>
-            <StyledTableCell align="center" style={{fontWeight: 'bold'}}>Data de sfarsit</StyledTableCell>
-            <StyledTableCell align="center" style={{fontWeight: 'bold'}}>Inlocuitor</StyledTableCell>
-            <StyledTableCell align="center" style={{fontWeight: 'bold'}}>Motiv</StyledTableCell>
-            <StyledTableCell align="center" style={{fontWeight: 'bold'}}>Angajat</StyledTableCell>
-            <StyledTableCell align="center" style={{fontWeight: 'bold'}}>Starea concediului</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name} >
-              <StyledTableCell component="th" scope="row" align="center">
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align="center">{row.dataInceput}</StyledTableCell>
-              <StyledTableCell align="center">{row.dataSfarsit}</StyledTableCell>
-              <StyledTableCell align="center">{row.inlocuitor}</StyledTableCell>
-              <StyledTableCell align="center">{row.motiv}</StyledTableCell>
-              <StyledTableCell align="center">{row.angajat}</StyledTableCell>
-              <StyledTableCell align="center">{row.stareConcediu}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <TabelConcediu rows={rows}></TabelConcediu>
+   
   );
 }
+
