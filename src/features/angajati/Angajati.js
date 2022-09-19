ca60ff0ca1fe985d8a9192d580456c014db92153
Andra Iancu <95624161+xAndruti@users.noreply.github.com>
2022-09-19 15:45:22 +0300

    stil pt tabel angajati

--- src/features/angajati/Angajati.js
+++ src/features/angajati/Angajati.js
@@ -1,32 +1,17 @@
-import * as React from 'react';
-import TabelAngajati from './TabelAngajati';
+import * as React from 'react'
+import TabelAngajati from './TabelAngajati'
 
-  function createData(nume, prenume, email, manager, echipa) {
-    return { nume, prenume, email, manager, echipa };
-  }
-  
-  const rows = [
-    createData('Popescu', 'Ioana', 'ioana@gmail.com', 'Popa Irina', 'IT'),
-    createData('Ionescu', 'Ana', 'ana@yahoo.ro', 'Popescu Mihai', 'Marketing'),
-    createData('Vasilescu', 'Mihai', 'mihai@gmail.com', 'Ionescu Cristina', 'Resurse Umane'),
-    createData('Enescu', 'Ion', 'ion@gmail.com', 'Soare Mihaela', 'Marketing'),
-    createData('Georgescu', 'Alina', 'alina@gmail.com', 'Enescu George', 'IT'),
-  ];
-
-export default function Angajati(){
-  return(
-    
-      <TabelAngajati rows={rows}></TabelAngajati>
-    
-  )
+function createData(nume, prenume, email, manager, echipa) {
+  return { nume, prenume, email, manager, echipa }
 }
+const rows = [
+  createData('Popescu', 'Ioana', 'ioana@gmail.com', 'Popa Irina', 'IT'),
+  createData('Ionescu', 'Ana', 'ana@yahoo.ro', 'Popescu Mihai', 'Marketing'),
+  createData('Vasilescu', 'Mihai', 'mihai@gmail.com', 'Ionescu Cristina', 'Resurse Umane'),
+  createData('Enescu', 'Ion', 'ion@gmail.com', 'Soare Mihaela', 'Marketing'),
+  createData('Georgescu', 'Alina', 'alina@gmail.com', 'Enescu George', 'IT')
+]
 
-
-
-
-
-
-  
-
-
-
+export default function Angajati() {
+  return <TabelAngajati rows={rows}></TabelAngajati>
+}
--- src/features/angajati/TabelAngajati.js
+++ src/features/angajati/TabelAngajati.js
@@ -1,51 +1,65 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import RanduriAngajati from './RanduriAngajati';
-import TableBody from '@mui/material/TableBody';
-
+import * as React from 'react'
+import { styled } from '@mui/material/styles'
+import Table from '@mui/material/Table'
+import TableCell, { tableCellClasses } from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
+import RanduriAngajati from './RanduriAngajati'
+import TableBody from '@mui/material/TableBody'
 import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core'
+import stilAngajati from './StilAngajati'
+
+const useStyles = makeStyles(stilAngajati)
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.success.dark,
-      color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
-    },
-  }));
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.success.dark,
+    color: theme.palette.common.white
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14
+  }
+}))
 
-export default function TabelAngajati({rows}){
-    return(
-<TableContainer component={Paper}>
-<Table sx={{ minWidth: 700 }} aria-label="customized table">
-  <TableHead>
-    <TableRow>
-      <StyledTableCell align="center">Nume</StyledTableCell>
-      <StyledTableCell align="center">Prenume</StyledTableCell>
-      <StyledTableCell align="center">Email</StyledTableCell>
-      <StyledTableCell align="center">Manager</StyledTableCell>
-      <StyledTableCell align="center">Echipa</StyledTableCell>
-    </TableRow>
+export default function TabelAngajati({ rows }) {
+  const stilTabel = useStyles()
+  return (
+    <TableContainer component={Paper}>
+      <div className={stilTabel.tabel}>
+        <Table sx={{ minWidth: 700 }} aria-label='customized table'>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align='center' style={{ fontWeight: 'bold' }}>
+                Nume
+              </StyledTableCell>
+              <StyledTableCell align='center' style={{ fontWeight: 'bold' }}>
+                Prenume
+              </StyledTableCell>
+              <StyledTableCell align='center' style={{ fontWeight: 'bold' }}>
+                Email
+              </StyledTableCell>
+              <StyledTableCell align='center' style={{ fontWeight: 'bold' }}>
+                Manager
+              </StyledTableCell>
+              <StyledTableCell align='center' style={{ fontWeight: 'bold' }}>
+                Echipa
+              </StyledTableCell>
+            </TableRow>
+          </TableHead>
 
-  </TableHead>
-  <TableBody>
-        {rows.map((row,i) => (   
-        <RanduriAngajati row={row} key={i}></RanduriAngajati>    
-         ))}
-        </TableBody>
-
-</Table>
-</TableContainer>
-    )
+          <TableBody>
+            {rows.map((row, i) => (
+              <RanduriAngajati row={row} key={i}></RanduriAngajati>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </TableContainer>
+  )
 }
-TabelAngajati.propTypes={
-    
-     rows: PropTypes.array.isRequired
+TabelAngajati.propTypes = {
+  rows: PropTypes.array.isRequired
 }
