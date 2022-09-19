d3acb3ea8e6489722ed8b02f63fa68901c6ac324
lieautman <dumi_cristi@yahoo.com>
2022-09-19 09:16:03 +0300

    proffile update

---
+++ src/features/profile/ProfileUserDataString.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+
+
+function ProfileUserDataString() {
+ //const addToast = useToast()
+ // addToast('Welcome', 'success')
+  return (
+    <Fragment>
+    <div>string</div>
+    </Fragment>
+  )
+}
+
+export default ProfileUserDataString
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -6,12 +6,14 @@ import CustomRoute from '../components/routing/CustomRoute'
 
 import { Forbidden, NotFound } from '@bit/totalsoft_oss.react-mui.kit.core'
 import Dashboard from 'features/dashboard/Dashboard'
+import Profile from 'features/profile/Profile'
 
 export default function AppRoutes() {
   return (
     <Switch>
       <CustomRoute isPrivate={false} exact path='/dashboard' component={Dashboard} />
       <Redirect exact from='/' to='/dashboard' />
+      <CustomRoute isPrivate={false} exact path='/profile' component={Profile} />
       <CustomRoute isPrivate={false} exact path='/forbidden' component={Forbidden} />
       <CustomRoute isPrivate={false} render={() => <NotFound title='PageNotFound'></NotFound>} />
     </Switch>
