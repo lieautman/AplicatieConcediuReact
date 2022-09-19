01e0672cabfbc6625024220c30ab0b8c6f34e08d
Andra Iancu <95624161+xAndruti@users.noreply.github.com>
2022-09-19 11:24:30 +0300

    tabel pagina angajati

--- src/features/angajati/Angajati.js
+++ src/features/angajati/Angajati.js
@@ -1,16 +1,82 @@
-import React, { Fragment } from 'react'
-import { Typography, Grid } from '@material-ui/core'
-import { useToast } from '@bit/totalsoft_oss.react-mui.kit.core'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-function Angajati() {
-  const addToast = useToast()
-  addToast('Welcome', 'success')
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.success.dark,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+  
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+  
+  const rows = [
+    createData('Popescu', 'Ioana', 'ioana@gmail.com', 'Popa Irina', 'IT'),
+    createData('Ionescu', 'Ana', 'ana@yahoo.ro', 'Popescu Mihai', 'Marketing'),
+    createData('Vasilescu', 'Mihai', 'mihai@gmail.com', 'Ionescu Cristina', 'Resurse Umane'),
+    createData('Enescu', 'Ion', 'ion@gmail.com', 'Soare Mihaela', 'Marketing'),
+    createData('Georgescu', 'Alina', 'alina@gmail.com', 'Enescu Geroge', 'IT'),
+  ];
+  
+
+export default function Angajati() {
   return (
-    <Fragment>
-      <Typography>Pagina angajati...</Typography>
-      <Grid>Internships are amazing.</Grid>
-    </Fragment>
-  )
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Nume</StyledTableCell>
+            <StyledTableCell align="right">Prenume</StyledTableCell>
+            <StyledTableCell align="right">Email</StyledTableCell>
+            <StyledTableCell align="right">Manager</StyledTableCell>
+            <StyledTableCell align="right">Echipa</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.calories}</StyledTableCell>
+              <StyledTableCell align="right">{row.fat}</StyledTableCell>
+              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+              <StyledTableCell align="right">{row.protein}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+   
+  );
 }
 
-export default Angajati
+
+
+
+  
+
+
+
