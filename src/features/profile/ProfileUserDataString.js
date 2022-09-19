c0bbfe964c87ae8eaf2dcb459a7d25b651af6507
lieautman <dumi_cristi@yahoo.com>
2022-09-19 13:24:18 +0300

    update profil

--- src/features/profile/ProfileUserDataString.js
+++ src/features/profile/ProfileUserDataString.js
@@ -1,15 +1,19 @@
 import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
 
 
 
-function ProfileUserDataString() {
+function ProfileUserDataString(props) {
  //const addToast = useToast()
  // addToast('Welcome', 'success')
   return (
     <Fragment>
-    <div>string</div>
+    <div className={props.idDat}>{props.text}</div>
     </Fragment>
   )
 }
-
+ProfileUserDataString.propTypes = {
+  text: PropTypes.object.isRequired,
+  idDat: PropTypes.object.isRequired,
+}
 export default ProfileUserDataString
