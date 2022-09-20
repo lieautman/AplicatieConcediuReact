8f56f6781008de0da7a7f46b137ccde7f7728f71
lieautman <dumi_cristi@yahoo.com>
2022-09-20 10:00:13 +0300

    update

--- src/features/profile/ProfileCss.js
+++ src/features/profile/ProfileCss.js
@@ -1,135 +1,143 @@
 const profileStyle = theme => {
-    return {
-        styleContainerMare:{
+  return {
+    styleContainerMare: {
+      [theme.breakpoints.down('sm')]: {
+        display: 'inline'
+      },
+      [theme.breakpoints.up('sm')]: {
+        display: 'flex'
+      }
+    },
+    textNumePrenume: {
+      color: '#C7C7C7',
+      fontSize: '16px'
+    },
+    containerProfileCard: {
+      background: '#FFFFFF',
+      boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.12)',
+      borderRadius: '14px',
+      marginTop: '1rem',
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
 
-            [theme.breakpoints.up('sm')]: {
-                display: 'inline',
-            },
-            [theme.breakpoints.up('sm')]: {
-                display: 'flex',
-            },
-        },
-        textNumePrenume:{
-            color:'#C7C7C7',
-            fontSize:'16px',
-        },
-        containerProfileCard:{
-            background: '#FFFFFF',
-            boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.12)',
-            borderRadius: '14px',
-            marginTop: '1rem',
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-        
-            width: '400px',
-            height: '96px',
-            left: 'calc(50% - 400px/2 - 332px)',
-        },
-        pozaProfilCard:{
-            width: '64px',
-            height: '64px',
-            margin: '16px',
-            padding: '0px',
-            borderRadius: '14px',
-            border: '5px black',
-            borderStyle: 'solid',
-        },
-        containerNumePrenumProfileCard:{
-            padding: '0',
-            margin: '16px',
-            marginRight: 'auto',
-        },
-        textNume:{
-            fontStyle: 'normal',
-            fontWeight: '700',
-            fontSize: '24px',
-            lineHeight: '45px',
-            color: '#202020',
-            padding: '0',
-            margin: '0',
-        },
-        textPrenume:{
-            fontStyle: 'normal',
-            fontWeight: '400',
-            fontSize: '14px',
-            lineHeight: '19px',
-            color: 'A5A5A5',
-            padding: '0',
-            margin: '0',
-        },
-        pozaEditProfilCard:{
-            height: '26px',
-            width: '26px',
-        },
-        containerAdresaEmailTelefonFunctieDataAngajare:{
-            display: 'flex',
-            padding: '0',
-            margin: '16px',
-            width: '300px',
-            marginTop: '1.4rem',
-        },
-        pozaAdresaEmailTelefon:{
-            height: '50px',
-            width: '50px',
-            borderRadius: '50%',
-        },
-        textAdresaEmailTelefonFunctiaDataAngajare:{
-            fontStyle: 'normal',
-            fontWeight: '600',
-            fontSize: '16px',
-            lineHeight: '22px',
-            color: '#202020',
-        },
+      width: '400px',
+      height: '96px',
+      left: 'calc(50% - 400px/2 - 332px)'
+    },
+    pozaProfilCard: {
+      width: '64px',
+      height: '64px',
+      margin: '16px',
+      padding: '0px',
+      borderRadius: '14px',
+      border: '5px black',
+      borderStyle: 'solid'
+    },
+    containerNumePrenumProfileCard: {
+      padding: '0',
+      margin: '16px',
+      marginRight: 'auto'
+    },
+    textNume: {
+      fontStyle: 'normal',
+      fontWeight: '700',
+      fontSize: '24px',
+      lineHeight: '45px',
+      color: '#202020',
+      padding: '0',
+      margin: '0'
+    },
+    textPrenume: {
+      fontStyle: 'normal',
+      fontWeight: '400',
+      fontSize: '14px',
+      lineHeight: '19px',
+      color: 'A5A5A5',
+      padding: '0',
+      margin: '0'
+    },
+    pozaEditProfilCard: {
+      height: '26px',
+      width: '26px'
+    },
+    containerAdresaEmailTelefonFunctieDataAngajare: {
+      display: 'flex',
+      padding: '0',
+      margin: '16px',
+      width: '300px',
+      marginTop: '1.4rem'
+    },
+    pozaAdresaEmailTelefon: {
+      height: '50px',
+      width: '50px',
+      borderRadius: '50%'
+    },
+    textAdresaEmailTelefonFunctiaDataAngajare: {
+      fontStyle: 'normal',
+      fontWeight: '600',
+      fontSize: '16px',
+      lineHeight: '22px',
+      color: '#202020'
+    },
 
+    styleCnpSerieNumarCi: {
+      [theme.breakpoints.up('lg')]: {
+        display: 'inline'
+      },
+      [theme.breakpoints.up('lg')]: {
+        display: 'flex'
+      }
+    },
 
-        styleCnpSerieNumarCi:{
-            [theme.breakpoints.up('lg')]: {
-                display: 'inline',
-            },
-            [theme.breakpoints.up('lg')]: {
-                display: 'flex',
-            },
-        },
+    styleContainerConcediileMele: {
+      margin: '1rem'
+    },
+    stilTabel: {
+      height: '19rem'
+    },
 
+    butonUploadAtasament: {
+      background: '#F5F5F5',
+      border: '2px dashed #C2C2C2',
+      borderRadius: '14px',
+      display: 'flex',
+      alignItems: 'center'
+    },
 
-        styleContainerConcediileMele:{
-            margin: '1rem',
-        },
-        stilTabel:{
-            height:'19rem',
-        },
+    stilADouaJumatateDePagina: {
+      overflow: 'scroll'
+    },
 
+    stilEditPageDivContainer1: {
+      alignItems: 'center',
+      position: 'relative'
+    },
+    stilEditPageDivContaineLeft: {
+      [theme.breakpoints.down('lg')]: {},
+      [theme.breakpoints.up('lg')]: { position: 'absolute' },
+      alignItems: 'center',
+      top: '10rem',
+      left: '10rem'
+    },
+    stilEditPageDivContainerRight: {
+      [theme.breakpoints.down('lg')]: {},
+      [theme.breakpoints.up('lg')]: { position: 'absolute' },
+      alignItems: 'center',
 
-
-        butonUploadAtasament:{
-            background: '#F5F5F5',
-            border: '2px dashed #C2C2C2',
-            borderRadius: '14px',
-            display: 'flex',
-            alignItems: 'center',
-        },
-
-        stilADouaJumatateDePagina:{
-            overflow: 'scroll',
-        },
-
-
-        stilEditPageDivContainer:{
-            display: 'flex',
-            flexDirection:'column',
-            alignItems: 'center',
-            margin:'auto',
-        },
-        stilEditPageDivContainer1:{
-            display: 'flex',
-            flexDirection:'row',
-            alignItems: 'center',
-        },
-        stilEditPageInput:{
-            padding:'0.22rem',
-        },
-        
+      top: '10rem',
+      right: '10rem'
+    },
+    stilEditPageInput: {
+      padding: '0.22rem'
+    },
+    stilEditPageInputButton: {
+      [theme.breakpoints.down('lg')]: {},
+      [theme.breakpoints.up('lg')]: { position: 'absolute' },
+      padding: '0.22rem',
+      top: '35rem',
+      left: '35rem'
     }
+  }
 }
 export default profileStyle
--- src/features/profile/ProfileEdit.js
+++ src/features/profile/ProfileEdit.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
 import TextField from '@mui/material/TextField'
@@ -15,45 +15,61 @@ function ProfileEdit({ stare, modifyDataProfile }) {
   //css
   const classes = useStyles()
 
-  useEffect(() => {
-    console.log(stare.textNume)
-  }, [stare.textNume])
-  
   return (
     <Fragment>
       <div className={classes.stilEditPageDivContainer1}>
-        <div className={classes.stilEditPageDivContainer}>
+        <div className={classes.stilEditPageDivContaineLeft}>
           <div className={classes.stilEditPageInput}>
-            <TextField label='Nume' defaultValue={stare.textNume} onChange={evt => modifyDataProfile('textNume', evt.target.value)}></TextField>
+            <TextField
+              label='Nume'
+              defaultValue={stare.textNume}
+              onChange={evt => modifyDataProfile('textNume', evt.target.value)}
+            ></TextField>
           </div>
           <div className={classes.stilEditPageInput}>
-            <TextField label='Prenume' defaultValue={stare.textPrenume} onChange={evt => modifyDataProfile('textNume', evt.target.value)}></TextField>
+            <TextField
+              label='Prenume'
+              defaultValue={stare.textPrenume}
+              onChange={evt => modifyDataProfile('textPrenume', evt.target.value)}
+            ></TextField>
           </div>
           <div className={classes.stilEditPageInput}>
-            <TextField label='Adresa' defaultValue={stare.textAdresaActuala} onChange={evt => modifyDataProfile('textNume', evt.target.value)}></TextField>
+            <TextField
+              label='Adresa'
+              defaultValue={stare.textAdresaActuala}
+              onChange={evt => modifyDataProfile('textAdresaActuala', evt.target.value)}
+            ></TextField>
           </div>
           <div className={classes.stilEditPageInput}>
-            <TextField label='Email' defaultValue={stare.textEmailActual} onChange={evt => modifyDataProfile('textNume', evt.target.value)}></TextField>
+            <TextField
+              label='Email'
+              defaultValue={stare.textEmailActual}
+              onChange={evt => modifyDataProfile('textEmailActual', evt.target.value)}
+            ></TextField>
           </div>
           <div className={classes.stilEditPageInput}>
             <TextField
               type='number'
               label='Telefon'
               defaultValue={stare.textTelefonActual}
-              onChange={evt => modifyDataProfile('textNume', evt.target.value)}
+              onChange={evt => modifyDataProfile('textTelefonActual', evt.target.value)}
             ></TextField>
           </div>
         </div>
-        <div className={classes.stilEditPageDivContainer}>
+        <div className={classes.stilEditPageDivContainerRight}>
           <div className={classes.stilEditPageInput}>
-            <TextField label='Functia' defaultValue={stare.textFunctiaActuala} onChange={evt => modifyDataProfile('textNume', evt.target.value)}></TextField>
+            <TextField
+              label='Functia'
+              defaultValue={stare.textFunctiaActuala}
+              onChange={evt => modifyDataProfile('textFunctiaActuala', evt.target.value)}
+            ></TextField>
           </div>
           <div className={classes.stilEditPageInput}>
             <TextField
               type='date'
               label='Data Angajare'
               defaultValue={stare.textDataNastereActuala}
-              onChange={evt => modifyDataProfile('textNume', evt.target.value)}
+              onChange={evt => modifyDataProfile('textDataAngajareActuala', evt.target.value)}
             ></TextField>
           </div>
           <div className={classes.stilEditPageInput}>
@@ -61,31 +77,31 @@ function ProfileEdit({ stare, modifyDataProfile }) {
               type='date'
               label='Data Nastere'
               defaultValue={stare.textDataNastereActuala}
-              onChange={evt => modifyDataProfile('textNume', evt.target.value)}
+              onChange={evt => modifyDataProfile('textDataNastereActuala', evt.target.value)}
             ></TextField>
           </div>
           <div className={classes.stilEditPageInput}>
             <TextField
               label='Cod Numeric Personal'
               defaultValue={stare.textTelefonActual}
-              onChange={evt => modifyDataProfile('textNume', evt.target.value)}
+              onChange={evt => modifyDataProfile('textCnpActual', evt.target.value)}
             ></TextField>
           </div>
           <div className={classes.stilEditPageInput}>
             <TextField
               label='Serie Si Numar CI'
               defaultValue={stare.textSerieNumarCiActual}
-              onChange={evt => modifyDataProfile('textNume', evt.target.value)}
+              onChange={evt => modifyDataProfile('textSerieNumarCiActual', evt.target.value)}
             ></TextField>
           </div>
-          <div className={classes.stilEditPageInput}>
-            {' '}
-            <Link to={'/profile'}>
-              <Button variant='contained' color='primary' size='large' className={classes.button} startIcon={<SaveIcon />}>
-                Save
-              </Button>
-            </Link>
-          </div>
+        </div>
+        <div className={classes.stilEditPageInputButton}>
+          {' '}
+          <Link to={'/profile'}>
+            <Button variant='contained' color='primary' size='large' className={classes.button} startIcon={<SaveIcon />}>
+              Save
+            </Button>
+          </Link>
         </div>
       </div>
     </Fragment>
