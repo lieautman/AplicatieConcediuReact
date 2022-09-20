8ca513c0d70b19c2a9eec867b21916921c5c209c
adelinazmarandoiu <adelinazmarandoiu@totalsoft.ro>
2022-09-20 17:46:58 +0300

    up

--- src/features/dashboard/adauga_angajat/Adauga_Angajatcss.js
+++ src/features/dashboard/adauga_angajat/Adauga_Angajatcss.js
@@ -4,29 +4,60 @@ import { findLastIndex } from 'ramda'
 const Adauga_Angajatcss = theme => {
   return {
     StyleBtn: {
+      [theme.breakpoints.down('lg')]: {
+        marginLeft: '13rem'
+      },
+      [theme.breakpoints.up('lg')]: {
+        marginLeft: '30rem'
+      },
       fontStyle: 'normal',
-      backgroundColor: '#1976d2',
-      color: 'white',
+      backgroundColor: '#26c6da',
+      color: ' white',
       border: 'none',
-      height: '50px',
-      width: '150px',
-      marginTop: '100px',
-      marginLeft: '350px',
+      height: '4rem',
+      width: '10rem',
+      marginTop: '5rem',
       borderRadius: '50px',
       fontSize: '15px'
     },
     containeradaugaaangajat: {
       color: '#1565c0',
       fontWeight: 'bold',
-      fontSize: '20px',
-      display: 'flex',
-      marginLeft: '50px'
+      fontSize: '5rem',
+      [theme.breakpoints.down('lg')]: {
+        display: 'inline'
+      },
+      [theme.breakpoints.up('lg')]: {
+        display: 'flex'
+      },
+      alignItems: 'center',
+      flexDirection: 'row',
+      margin: '0',
+      padding: '0',
+      marginTop: '3rem'
     },
-    containeradaugaangajatleft: {
-      marginLeft: '10px'
-    },
+    containeradaugaangajatleft: { marginLeft: '10rem' },
     containeradaugaangajatright: {
-      marginLeft: '200px'
+      [theme.breakpoints.down('lg')]: {
+        marginLeft: '15rem'
+      },
+      [theme.breakpoints.up('lg')]: {
+        marginLeft: '20rem'
+      }
+    },
+    StyleTxt: {
+      color: ' #05241d',
+      fontWeight: 'bold',
+      fontSize: '20px',
+      alignSelf: 'center'
+    },
+    Combobox: {
+      width: 250,
+      marginTop: '1rem'
+    },
+    TextField: {
+      marginTop: '1rem',
+      width: 250
     }
   }
 }
