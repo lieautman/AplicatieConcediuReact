f9d3c9115eae32b6d1b4b764d10921ca8b0b3d98
elena.craciun <elena.craciun@TS1542>
2022-09-21 17:47:25 +0300

    -stiluri echipa

---
+++ src/features/dashboard/StilCarduri.js
@@ -0,0 +1,11 @@
+import { AdbRounded } from '@material-ui/icons'
+
+const carduri = theme => {
+  return {
+    displayCarduri: {
+      justifyContent: 'space-around',
+      marginTop: '1rem'
+    }
+  }
+}
+export default carduri
