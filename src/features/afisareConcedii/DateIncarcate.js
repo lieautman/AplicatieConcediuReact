e9b9658d42b895fc19fc3fd40948a9b3d2dca598
elena.craciun <elena.craciun@TS1542>
2022-09-26 10:10:00 +0300

    rfer

--- src/features/afisareConcedii/DateIncarcate.js
+++ src/features/afisareConcedii/DateIncarcate.js
@@ -29,7 +29,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 
 export default function DateIncarcate(props) {
   const { row, setareId, concediiInAsteptareaAprobarii, idRand } = props
-
+  console.log(idRand)
   return (
     <StyledTableRow>
       {concediiInAsteptareaAprobarii && (
